3cbcdebd902cfbe478e63818b8d2760000f3bca9
Riya <riiyyakk@gmail.com>
2026-04-01 21:28:53 -0700

    Add files via upload

--- StudentLifeSim.java
+++ StudentLifeSim.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
 import java.util.Random;
-import javax.lang.model.util.ElementScanner14;
+
 
 public class StudentLifeSim {
 
@@ -25,32 +25,36 @@ public class StudentLifeSim {
 
             if(Grades >= 10 && Social <= 3 && WellBeing <= 5 && WellBeing > 0 && EXTRACURRICULAR <= 5)
             {
-                System.out.println("ENDING 1/6: LOST ENDING\n You got into a good college however you don't know what you're passionate about. \n You feel lost and lonely. Was studying this much really worth it?");
+                System.out.println("ENDING 1/7: LOST ENDING\n You got into a good college however you don't know what you're passionate about. \n You feel lost and lonely. Was studying this much really worth it?");
             }
             else if(WellBeing <= 0)
             {
-                System.out.println("ENDING 2/6: DEPRESSION ENDING\n You drove your health in the pavement. You got so sad you never applied to college. Remeber to have let loose and relax once in a while.");
+                System.out.println("ENDING 2/7: DEPRESSION ENDING\n You drove your health in the pavement. You got so sad you never applied to college. Remeber to let loose and relax once in a while.");
+            }
+            else if(Grades >= 5 && EXTRACURRICULAR >=5 && WellBeing >0)
+            {
+                System.out.println("ENDING 7/7: IVY LEAGUE ENDING \n Amazing job! You focused on your passion and your drive with academics. Pairing these two truly shows you have a goal you want to achieve in the future! :) Just make sure you don't get burnt out! Keep an eye on that mental health!");
             }
             else if(Fitness >= 10 && Grades >= 8)
             {
-                System.out.println("ENDING 3/6: ATHLETIC SCHOLARSHIP ENDING\n Congrats! You got into an amazing college doing the sport you love :) Can't wait to see you on TV playing at the professional level!");
+                System.out.println("ENDING 3/7: ATHLETIC SCHOLARSHIP ENDING\n Congrats! You got into an amazing college doing the sport you love :) Can't wait to see you on TV playing at the professional level!");
             }
             else if(Social >= 10 && WellBeing >= 7)
             {
-                System.out.println("ENDING 4/6: NETWORKED ENDING \n You went to a low ranked college to be with your friends, however turns out that wasn't a bad idea! You networked your way into an amzazing intership thanks to your social skills!");
+                System.out.println("ENDING 4/7: NETWORKED ENDING \n You went to a low ranked college to be with your friends, however turns out that wasn't a bad idea! You networked your way into an amzazing intership thanks to your social skills!");
             }
             else if(WellBeing>= 10)
             {
-                System.out.println("ENDING 5/6: ENLIGHTNED ENDING\n You became mentally so healthy that you have the mindset of a monk. You've decided to take a gap year and travel the world connecting to nature and spiritualism. You live life against society norms.");
+                System.out.println("ENDING 5/7: ENLIGHTNED ENDING\n You became mentally so healthy that you have the mindset of a monk. You've decided to take a gap year and travel the world connecting to nature and spiritualism. You live life against society norms.");
 
             }
             else if(EXTRACURRICULAR >= 10)
             {
-                System.out.println("ENDING 6/6: PASSION ENDING\n GREAT JOB, you got into an amazing college thanks to your pursuit towards your passion that related to your career. You are excited to do the thing you love for the rest of your life :D!");
+                System.out.println("ENDING 6/7: PASSION ENDING\n GREAT JOB, you got into an amazing college thanks to your pursuit towards your passion that related to your career. You are excited to do the thing you love for the rest of your life :D!");
 
             }
             else{
-                System.out.println("ENDING 0/6: Average Ending\n nice job, you got into an average-good college, lived a medicore life. Life is Simple but Peaceful. \n Wanna try getting all Endings?");
+                System.out.println("ENDING 0/7: Average Ending\n nice job, you got into an average-good college, lived a medicore life. Life is Simple but Peaceful. \n Wanna try getting all Endings?");
             }
 
 
@@ -106,6 +110,9 @@ public class StudentLifeSim {
         Grades += increaseBy;
         daysLeft -= 1;
 
+        String[] studyVerbs = {"studied a really long time", "pulled an all nighter", "absorbed your physic's book", "digested the periodic table", "watched endless videos on calculus", "wrote 30 essays"};
+        int randVerbs = rand.nextInt(5);
+        System.out.println(" \n   You successfully " + studyVerbs[randVerbs] +"\n");
         showStats();
     }
     public static void Party()
@@ -117,6 +124,10 @@ public class StudentLifeSim {
         Social += increaseBy;
         daysLeft -= 1;
 
+        String[] partyVerbs = {"went to a party", "spent a day at the mall with friends", "met the love of your life", "explored the city with new friends", "acquired a new best friend", "called and played video games with friends"};
+        int randVerbs = rand.nextInt(6);
+        System.out.println(" \n   You successfully " + partyVerbs[randVerbs] +"\n");
+
         showStats();
     }
     public static void Meditate()
@@ -126,6 +137,9 @@ public class StudentLifeSim {
 
         WellBeing += increaseBy;
         daysLeft -= 1;
+        String[] meditateVerbs = {"took a long nap", "learned and ate healthy meals", "journaled all your pent up emotions", "went outside in nature and meditated", "drank some medicine", "went to the doctor and therapy"};
+        int randVerbs = rand.nextInt(6);
+        System.out.println(" \n   You successfully " + meditateVerbs[randVerbs] +"\n");
 
         showStats();
     }
@@ -137,12 +151,22 @@ public class StudentLifeSim {
         Fitness += increaseBy;
         daysLeft -= 1;
 
+        String[] gymVerbs = {"grinded in the gym", "ran 5 miles", "played a basketball tournament", "did 50 karate lessons", "swam 50 laps", "biked across two cities"};
+        int randVerbs = rand.nextInt(6);
+        System.out.println(" \n   You successfully " + gymVerbs[randVerbs] +"\n");
+
         showStats();
     }
     public static void Clubs()
     {
         EXTRACURRICULAR += 1;
         daysLeft -= 1;
+
+        String[] clubsVerbs = {"joined a STEM club", "volunteered at the place that needed help", "researched more about your dream career", "built a passion project", "acquired a leadership position in your club", "got a job"};
+        
+        Random rand = new Random();
+        int randVerbs = rand.nextInt(6);
+        System.out.println(" \n   You successfully " + clubsVerbs[randVerbs] +"\n");
 
         showStats();
     }
@@ -162,4 +186,3 @@ public class StudentLifeSim {
 
     }
 }
-
